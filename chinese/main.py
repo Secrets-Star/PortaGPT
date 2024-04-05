97d43c867baa34c237be873425f41ce8fe2891b1
Secrets-Star <161010249+Secrets-Star@users.noreply.github.com>
2024-04-05 10:33:42 +0800

    Update main.py

--- chinese/main.py
+++ chinese/main.py
@@ -3,18 +3,25 @@ from gpiozero import Button
 import urllib.request
 import os
 import requests
+from TTS.api import TTS
 
 button = Button(2)
+led = LED(21)
 
 while True:
     while button.is_pressed:
+        #shine led
+        led.on()
         # record
         os.system("arecord --format=cd tempquest.wav")
+    led.off()
     # speech to text
-
+    os.system("vosk-transcriber --model-name vosk-model-cn-0.22 -i tempquest.wav -o tempquest.txt")
+    os.system("rm tempquest.wav")
+    with open("tempquest.txt", "r") as f:
+        prompt = f.read()
     # rest api
     url = "http://localhost:11434/api/generate"
-    prompt = input("Enter prompt: ")
     data = "{\"model\": \"portagpt\", \"prompt\":\"" + prompt + "\"}"
     headers = { 'Content-Type': 'application/json' }
     req = urllib.request.Request(url, data.encode('utf-8'), headers)
@@ -23,12 +30,11 @@ while True:
         resp = result.decode('utf-8')
 
     # text to speech
-    from TTS.api import TTS
 
     # Initialize the TTS object
     tts = TTS("tts_models/en/vctk/vits")
 
-    # Synthesize speech
+    # Synthesize speech 
     tts.tts_to_file(resp, "output.wav")
 
     # play the audio
